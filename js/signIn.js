9c812d32cca7f4560d1df1ec025e466e43507a76
Usama Navid <usamanavid098@gmail.com>
2024-02-13 22:56:37 +0000

    Add front end of SIGN IN page along with its css and html

---
+++ js/signIn.js
@@ -0,0 +1,4 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    // Here you can add code to handle form submission, like AJAX request to authenticate user, etc.
+  });
