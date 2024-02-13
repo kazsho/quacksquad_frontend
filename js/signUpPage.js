af2988aa1427ed6414f00f69557b0cee8a13a262
Usama Navid <usamanavid098@gmail.com>
2024-02-13 20:31:39 +0000

    Add front end of SIGN UP page along with its css and html

---
+++ js/signUpPage.js
@@ -0,0 +1,9 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // You can add form submission logic here
+  });
+  
+  document.getElementById('login-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    // You can add logic to redirect to the login page here
+  });
